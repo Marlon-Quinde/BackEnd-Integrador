888e5689c4556514a2c3ff0819f53558a1dcfae3
Marlon Quinde <mquinde@viamatica.com>
2024-12-19 20:06:15 -0500

    feat(FS/PATH): :sparkles: Modificacion del archivo repository para permitir la lectura del archivo users.json

--- src/modules/auth/repository.ts
+++ src/modules/auth/repository.ts
@@ -1,23 +1,33 @@
 import { UserI } from "../../interfaces/Auth.interface";
+import fs from 'fs/promises';
+import path from 'path';
 
-const users: UserI[] = [];
-
+const dataFilePath = path.join('src', 'data', 'users.json');
 export default class AuthRepository {
 
+    async readUsers(): Promise<UserI[] | Array<UserI>> {
+        try {
+            const data = await fs.readFile(dataFilePath, 'utf-8')
+            return JSON.parse(data)
+        } catch (error) {
+            return []
+        }
+    }
+
+    async writeUsers(users: UserI[]): Promise<void>{
+        await fs.writeFile(dataFilePath, JSON.stringify(users, null, 2), 'utf-8')
+    }
+
     async createUser(user: UserI): Promise<UserI> {
+        const users = await this.readUsers();
         users.push(user);
+        await this.writeUsers(users);
         return user;
     }
 
-    // ? Este codigo sirve para tal cosa cosa
     async findByUsername(username: string): Promise<UserI | undefined> {
+        const users = await this.readUsers();
         return users.find((user) => user.username === username);
     }
 
-    // async findByUsernameDosPuntocero(username: string): Promise<UserI | undefined> {
-    //     return users.find((user) => user.username === username);
-    // }
-
-    // ? ==     Comparación Debil   1 == '1' // VERDADERO
-    // ? ===    Comparación Fuerte  1 === '1' // FALSO
 }
